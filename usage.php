9033dc5865f813cb8dcf72f2ee54a83deb038951
royall81 <buijs.roed@gmail.com>
2022-09-26 16:26:59 +0200

    update usage page

--- usage.php
+++ usage.php
@@ -1,37 +1,62 @@
 <div class="container my-5" style="min-height:200px;">
-     <div class="row pt-0">
+    <div class="row pt-0">
 
-          <div class="col-12 text-center pb-5" style="font-size:27px;">
-               User guides for <b  style="font-weight:800;"> OUR PRODUCTS</b>
-          </div>
+        <div class="col-12 text-center pb-5" style="font-size:27px;">
+            User guides for <b  style="font-weight:800;"> OUR PRODUCTS</b>
+        </div>
+
+        <div class="col-12">
+            <h2>Mycorrhiza</h2>
+        </div>
+
+        <div class="col-12">
+            <a href="/user-guide-mycorrhiza-mix" style="color:black;">
+                <div class="media col-11 offset-1">
+                    <img src="/img/mycorrhiza.jpg" width="64" class="mr-3" alt="Mycorrhiza">
+                    <div class="media-body align-self-center">
+                        <h3 style="font-weight:200;">Mycorrhyza Mix</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-12">
+            <a href="/user-guide-mycorrhiza-mix" style="color:black;">
+                <div class="media col-11 offset-1">
+                    <img src="/img/rhyzobac.jpg" width="64" class="mr-3" alt="Mycorrhiza">
+                    <div class="media-body align-self-center">
+                        <h3 style="font-weight:200;">Mycorrhiza Mix</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-12 pt-3">
+            <h2>Mycorrhiza</h2>
+        </div>
+
+        <div class="col-12">
+            <a href="/user-guide-mycorrhiza-mix" style="color:black;">
+                <div class="media col-11 offset-1">
+                    <img src="/img/mycorrhiza.jpg" width="64" class="mr-3" alt="Mycorrhiza">
+                    <div class="media-body align-self-center">
+                        <h3 style="font-weight:200;">Mycorrhyza Mix</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-12">
+            <a href="/user-guide-mycorrhiza-mix" style="color:black;">
+                <div class="media col-11 offset-1">
+                    <img src="/img/rhyzobac.jpg" width="64" class="mr-3" alt="Mycorrhiza">
+                    <div class="media-body align-self-center">
+                        <h3 style="font-weight:200;">Mycorrhiza Mix</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
 
 
-          <div class="col-12 col-md-6">
-               <div class="col-12 text-center" style="min-height:320px;">
-                    <a href="/user-guide-mycorrhiza-mix"><img class="mx-auto img-fluid"  src="/img/mycorrhiza.jpg" /></a>
-               </div>
-               <div class="col-12 text-center pb-5 pb-md-0" style="font-family: 'Montserrat', sans-serif; font-weight:600; font-size:24px;">
-                    Mycorrhiza
-               </div>
-          </div>
-
-          <div class="col-12 col-md-6">
-               <div class="col-12 text-center" style="min-height:320px;">
-                    <a href="/user-guide-rhizobac-mix"><img class="mx-auto img-fluid" src="/img/rhyzobac.jpg" /></a>
-               </div>
-               <div class="col-12 text-center pb-5 pb-md-0" style="font-family: 'Montserrat', sans-serif; font-weight:600; font-size:24px;">
-                    Rhizobac
-               </div>
-          </div>
-<!--
-          <div class="col-12 col-md-4">
-               <div class="col-12 text-center" style="min-height:320px;">
-                    <a href="/user-guide-plant-fertilizer"><img class="mx-auto img-fluid" src="/img/coming-soon.jpg" /></a>
-               </div>
-               <div class="col-12 text-center pb-5 pb-md-0" style="font-family: 'Montserrat', sans-serif; font-weight:600; font-size:24px;">
-                    Organic Plant Feed
-               </div>
-          </div>
--->
-     </div>
+    </div>
 </div>
